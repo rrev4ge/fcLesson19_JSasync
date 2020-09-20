4c4d1251f4b904a585da7609bded2027c0299c9e
rrev <68103311+rrev4ge@users.noreply.github.com>
2020-09-20 09:42:58 +0300

    bug fix class tasks

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,60 +6,30 @@ btn.addEventListener("click", (event) => {
   alert("Alert")
 })
 
-
-function logNum(i = 1) {
-    if (i === 21) {
-      return;
-    }
-    console.log(i);
-    i++;
-    setTimeout(logNum(i), 100000000);
-}
-
-//logNum();
-
-let timerId;
-function logNum () {
-    timerId = setTimeout(function() {
-      console.log(i);
-    }, 1000);
-};
-    clearTimeout(timerId);
-};
-
-// setInterval(()=>{
-//   i++;
-// },100);
-
-// clearInterval(setIi);
-
-
-const p1 = new Promise((resolve, reject) => {
-  resolve("RESOLVED PROMISE");
-});
-
-console.log(p1);
-
-p1.then(
-  (string) => {
-    console.log("then result: ", string);
-    return new Promise((resolve, reject) => {
-      resolve(string);
-    });
-  },
-  (err) => {
-    console.error(err);
-  })
-  .then(
-    (test) => {
-      console.log(test);
-    })
+logNumTimeout();
+logNumInterval();
 
 fetch("../../user.json")
   .then((res) => res.json())
   .then((users) => createList(users))
   .catch(console.error);
 
+function logNumTimeout(num = 1) {
+    if(num <= 20) {  
+      console.log(num);     
+      setTimeout(logNumTimeout, 100, num + 1);
+    }
+}
+
+function logNumInterval(num = 1) {
+    let timerId = setInterval(function() {
+      console.log(num);
+      if (num === 20) {
+        clearInterval(timerId);
+      }
+      num++;
+    }, 100);
+}
 
 function createList(users) {
   const root = document.getElementById("root");
